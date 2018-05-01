a5d68cff164fb22dc808ad6c7c8dbb91c00b1ef4
MCyunpeng98 <84106749@qq.com>
2018-05-01 18:09:23 +0800

    手机版教务处功能基本完善

--- src/LoginHnsfxyPhone_Grap.php
+++ src/LoginHnsfxyPhone_Grap.php
@@ -3,6 +3,8 @@
 namespace MCyunpeng98\LoginHnsfxy_Grap;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\RequestOptions;
 
 class LoginHnsfxyPhone_Grap{
     private $homeUri = 'http://211.70.176.123/wap/';
@@ -16,25 +18,70 @@ class LoginHnsfxyPhone_Grap{
     {
         if ($this->client == NULL){
             $client = new Client([
-                'base_uri' => $this->loginUri,
+                'base_uri' => $this->homeUri,
                 'timeout' => 5,
             ]);
         }
         $this->client = $client;
     }
 
-    public function LoginHnsfxyPhone($studeentNum, $idCard){
+    /**
+     * Step.1 登录手机版的教务处
+     * @param $studentNum 学号
+     * @param $idCard 身份证号
+     */
+    public function LoginHnsfxyPhone($studentNum, $idCard){
         $reponse = null;
-        $this->cookies = new \GuzzleHttp\Cookie\CookieJar;
-            $reponse =$this->client->request('post',$this->loginUri,[
-                'cookiees' => $this->cookies,
-                'form_parms' => [
-                    'xh' => $studeentNum,
+        $this->cookies = new CookieJar;
+        $reponse =$this->client->request('post',$this->loginUri,[
+                'cookies' => $this->cookies,
+                'form_params' => [
+                    'xh' => $studentNum,
                     'sfzh' => $idCard,
                 ]
-            ]);
+        ]);
         $content = mb_convert_encoding($reponse->getBody(), 'UTF-8', 'gbk');
-        print_r($content);
-        die();
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getStudentInfo(){
+        $reponse = null;
+        $d = [];
+        $reponse = $this->client->get($this->studentInfoUri,[
+            RequestOptions::COOKIES => $this->cookies
+        ]);
+        $content = mb_convert_encoding($reponse->getBody()->getContents(), 'UTF-8', 'gbk');
+//        echo '<textarea>'.((string) $content).'</textarea>';
+
+        if (preg_match('/<IMG SRC="\.\.\/dbsdb\/tp\.asp\?xh=(\d{10})" width="120" height="160">[\s\S]+?<font color=red>(.+)<\/font>[\s\S]+?班级[\s\S]+?<td align="center" width="170" height="22" valign="middle">(.+)<\/td>[\s\S]+?<td align="center" width="150" height="22" valign="middle">(.+)<\/td>[\s\S]+?政治面貌<\/font><\/td>[\s\S]+?<td align="center" width="170" height="22" valign="middle">(.+)<\/td>/'
+                                ,$content,$d))
+        {
+            $info = [
+                //学号
+                'student_num'   => $d[1],
+                //姓名
+                'student_name'  => $d[2],
+                //二级学院
+                'department'    => $d[3],
+                //专业+班级
+                'student_class' => $d[4],
+                //生日
+                'birthday'      => $d[5]
+            ];
+            $this->currentStudentNum = $info['student_num'];
+            return $info;
+        }
+    }
+
+    public function savePhoto($path =''){
+        if ($path == '')
+            $path = tempnam("./src/photo","MC_");
+            $this->client->get($this->photoUri.$this->currentStudentNum, [
+                RequestOptions::SINK=>$path,
+                RequestOptions::HTTP_ERRORS => false
+            ]);
+        return $path;
     }
 }
