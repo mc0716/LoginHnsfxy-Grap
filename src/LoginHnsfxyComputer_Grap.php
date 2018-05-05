9d2d9120b182b4e612afd6ef31a9e0dd455b8502
MCyunpeng98 <84106749@qq.com>
2018-05-05 18:50:45 +0800

    修改正则获取的信息

--- src/LoginHnsfxyComputer_Grap.php
+++ src/LoginHnsfxyComputer_Grap.php
@@ -85,7 +85,7 @@ class LoginHnsfxyComputer_Grap{
         }
 
         $content = mb_convert_encoding($reponse->getBody()->getContents(), 'UTF-8', 'gbk');
-        if (preg_match('/<img id="xszp" src="(.+)" alt="照片" align="AbsMiddle" border="0" height="210" width="150" \/>[\s\S]+?<TD><span id="lbl_xb">(.+)<\/span><\/TD>[\s\S]+?<TD><span id="lbl_xy">(.+)<\/span><\/TD>[\s\S]+?<TD><span id="lbl_zymc">(.+)<\/span><\/TD>/',$content,$d)){
+        if (preg_match('/<img id="xszp" src="(.+)" alt="照片" align="AbsMiddle" border="0" height="210" width="150" \/>[\s\S]+?<TD><span id="lbl_xb">(.+)<\/span><\/TD>[\s\S]+?<TD><span id="lbl_xy">(.+)<\/span><\/TD>[\s\S]+?<TD><span id="lbl_xzb">(.+)<\/span><\/TD>/',$content,$d)){
             $this->photoUri = $d[1];
             $info = [
                 //学生姓名
@@ -98,7 +98,7 @@ class LoginHnsfxyComputer_Grap{
                 'student_xy' => $d[3],
                 //学生专业+班级
                 'student_zybj' => $d[4],
-                //学生教务处密码
+                //学生教务处密码a
                 'student_password' => $this->studentPassword
             ];
             return $info;
