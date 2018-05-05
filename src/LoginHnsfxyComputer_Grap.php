923bdfe30445517faff755d8908174fccbfc6692
MCyunpeng98 <84106749@qq.com>
2018-05-05 15:01:47 +0800

    添加保存图片，有问题（展厅使用）

--- src/LoginHnsfxyComputer_Grap.php
+++ src/LoginHnsfxyComputer_Grap.php
@@ -10,13 +10,12 @@ class LoginHnsfxyComputer_Grap{
     private $homeUri = 'http://211.70.176.123/';
     private $loginUri = 'default2.aspx';
     private $studentInfoUri = '/xsgrxx.aspx?xh=';
-    private $photoUri = 'readimagexs.aspx?xh=';
     private $captchaUri = 'http://211.70.176.123/CheckCode.aspx';
     private $captchaInfouri = 'http://www.mq1314.cn:5000/image_to_label';
+    private $photoUri = null;
     private $studentNum = null;
     private $studentPassword =null;
     private $studentName = null;
-    private $currentStudentNum = '';
     private $VIEWSTATE= null;
     private $captcha = null;
     public $client = null;
@@ -77,29 +76,40 @@ class LoginHnsfxyComputer_Grap{
         }
 
         $content = mb_convert_encoding($reponse->getBody()->getContents(), 'UTF-8', 'gbk');
-        if (preg_match('/<TD><span id="lbl_xb">(.+)<\/span><\/TD>[\s\S]+?<TD><span id="lbl_xy">(.+)<\/span><\/TD>[\s\S]+?<TD><span id="lbl_zymc">(.+)<\/span><\/TD>/',$content,$d)){
+        echo $content;
+        if (preg_match('/<img id="xszp" src="(.+)" alt="照片" align="AbsMiddle" border="0" height="210" width="150" \/>[\s\S]+?<TD><span id="lbl_xb">(.+)<\/span><\/TD>[\s\S]+?<TD><span id="lbl_xy">(.+)<\/span><\/TD>[\s\S]+?<TD><span id="lbl_zymc">(.+)<\/span><\/TD>/',$content,$d)){
+            $this->photoUri = $d[1];
             $info = [
                 //学生姓名
                 'student_name' => $this->studentName,
                 //学生学号
                 'student_num' =>$this->studentNum,
                 //学生性别
-                'student_xb' => $d[1],
+                'student_xb' => $d[2],
                 //学生学院
-                'student_xy' => $d[2],
+                'student_xy' => $d[3],
                 //学生专业+班级
-                'student_zybj' => $d[3],
+                'student_zybj' => $d[4],
                 //学生教务处密码
                 'student_password' => $this->studentPassword
             ];
             return $info;
         }
     }
-
+    /**
+     * 暂时有问题
+     */
 //    public function savePhoto($path =''){
 //        if ($path == '')
-//            $path = './src/public/photo/'."$this->currentStudentNum".'.jpg';
-//        $this->client->get('http://211.70.176.123/CheckCode.aspx', [
+//            $path = './src/public/photo/'."$this->studentNum".'.JFIF';
+//        $this->client->get($this->homeUri."$this->photoUri", [
+//            'allow_redirects'=> [
+//                'max'             => 50,
+//                'strict'          => false,
+//                'referer'         => true,
+//                'protocols'       => ['http', 'https'],
+//                'track_redirects' => false
+//            ],
 //            RequestOptions::SINK=>$path,
 //            RequestOptions::HTTP_ERRORS => false
 //        ]);
